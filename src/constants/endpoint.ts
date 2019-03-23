854f4f660f21f9507ba269bac2d436232b109d16
Andrew Gionfriddo <andrew.gionfriddo@gmail.com>
2019-03-23 14:07:33 -0400

    Changes endpoint to be better

--- src/constants/endpoint.ts
+++ src/constants/endpoint.ts
@@ -1,59 +1,14 @@
-const ROOT_URL = "https://atlas-back-end.herokuapp.com/";
+const ROOT_URL = process.env.ROOT_URL || "http://localhost:10013";
 
-// create constants for each service URL here and replace DEV_URL
-const DEV_URL = "http://localhost:10013";
 const URL = {
-  development: {
-    personalityEvaluations: `${DEV_URL}/personality-evaluations`,
-    applicants: `${DEV_URL}/applicants`,
-    industries: `${DEV_URL}/industries`,
-    skills: `${DEV_URL}/skills`,
-    certifications: `${DEV_URL}/certifications`,
-    industrySectors: `${DEV_URL}/industrySectors`,
-    me: `${DEV_URL}/me`,
-    login: `${DEV_URL}/login`
-  },
-  staging: {
-    personalityEvaluations: `${DEV_URL}/personality-evaluations`,
-    applicants: `${DEV_URL}/applicants`,
-    industries: `${DEV_URL}/industries`,
-    skills: `${DEV_URL}/skills`,
-    certifications: `${DEV_URL}/certifications`,
-    industrySectors: `${DEV_URL}/industrySectors`,
-    me: `${DEV_URL}/me`,
-    login: `${DEV_URL}/login`
-  },
-  production: {
-    personalityEvaluations: `${ROOT_URL}/personality-evaluations`,
-    applicants: `${ROOT_URL}/applicants`,
-    industries: `${ROOT_URL}/industries`,
-    skills: `${ROOT_URL}/skills`,
-    certifications: `${ROOT_URL}/certifications`,
-    industrySectors: `${ROOT_URL}/industrySectors`,
-    me: `${ROOT_URL}/me`,
-    login: `${ROOT_URL}/login`
-  }
+  personalityEvaluations: `${ROOT_URL}/personality-evaluations`,
+  applicants: `${ROOT_URL}/applicants`,
+  industries: `${ROOT_URL}/industries`,
+  skills: `${ROOT_URL}/skills`,
+  certifications: `${ROOT_URL}/certifications`,
+  industrySectors: `${ROOT_URL}/industrySectors`,
+  me: `${ROOT_URL}/me`,
+  login: `${ROOT_URL}/login`
 };
 
-enum Env {
-  dev = "development",
-  stage = "staging",
-  prod = "production"
-}
-
-function setEnv(environmentString: string): Env {
-  switch (environmentString) {
-    case "development":
-      return Env.dev;
-    case "staging":
-      return Env.stage;
-    case "production":
-      return Env.prod;
-    default:
-      return Env.dev;
-  }
-}
-
-const environment = setEnv(process.env.NODE_ENV || "development");
-
-export default URL[environment];
+export default URL;
