4237f34df5b502f5f22a5e1924dcf0fb12394fbc
Andrew Gionfriddo <andrew.gionfriddo@gmail.com>
2019-04-21 20:41:15 -0400

    Ejects webpack config and adds env variables

--- src/config/aws.ts
+++ src/config/aws.ts
@@ -3,7 +3,7 @@ import { config, S3, Credentials } from 'aws-sdk';
 const bucketName = 'atlas-profile-pictures';
 const bucketRegion = 'us-east-1';
 const accessKeyId = process.env.ACCESS_KEY_ID || '';
-const secretAccessKey = process.env.ACCESS_KEY_ID || '';
+const secretAccessKey = process.env.SECRET_ACCESS_KEY || '';
 
 config.update({
   region: bucketRegion,
